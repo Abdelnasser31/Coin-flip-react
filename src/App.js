b9f4e6a7fe9b1d761d3bc83c876dc32429e2f033
Abdelnasser <abdelnasser.kh10@gmail.com>
2019-11-02 22:01:40 +0300

    add the game file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import './App.css';
 import Coin from './Coin';
+import Game from './Game';
 
 function App() {
   return (
     <div className="App">
-      <Coin />
+      <Game />
     </div>
   );
 }
--- src/Coin.js
+++ src/Coin.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react'
 // https://i.colnect.net/f/1177/395/25_Pounds.jpg
-class Coin extends Component{
-    static defaultProps = {
-        image: 'https://i.colnect.net/f/1177/396/25-Pounds-back.jpg'
-    }
-    render() {
-        return (<img src={this.props.image} alt={this.props.face} />)
-    }
+class Coin extends Component {
+  static defaultProps = {
+    image: 'https://i.colnect.net/f/1177/396/25-Pounds-back.jpg'
+  }
+  render() {
+
+    return (
+      <div>
+        <img src={this.props.info.imgSrc} alt={this.props.info.side}/>
+      </div>
+    )
+  }
 }
 
 export default Coin
