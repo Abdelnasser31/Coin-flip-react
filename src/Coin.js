b10c5afe105b043761cedd2d8f6b34a92d738e84
Abdelnasser <abdelnasser.kh10@gmail.com>
2019-11-02 17:12:44 +0300

    change props names in Coin

--- src/Coin.js
+++ src/Coin.js
@@ -2,10 +2,10 @@ import React, {Component} from 'react'
 // https://i.colnect.net/f/1177/395/25_Pounds.jpg
 class Coin extends Component{
     static defaultProps = {
-        face: 'https://i.colnect.net/f/1177/396/25-Pounds-back.jpg'
+        image: 'https://i.colnect.net/f/1177/396/25-Pounds-back.jpg'
     }
     render() {
-        return (<img src={this.props.face} alt="writing" />)
+        return (<img src={this.props.image} alt={this.props.face} />)
     }
 }
 
