3cf5ec155e7e9da50bbda4a8c7b1ec8fd52c4380
Nasser <abdelnasser.kh10@gmail.com>
2020-09-05 14:47:41 +0300

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# Coin Flip
-This project is an exercise in "React bootcamp course" by Colt Steele.
-
-## Objectives
-Practice working with props, defaultProps, state, and simple click events
---
+++ precache-manifest.90c79d8d6e55884228130c7e05d19482.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42669d29d81c8f937b057f0f7c036771",
+    "url": "/flip-coin/index.html"
+  },
+  {
+    "revision": "faf143a2d91cf884c058",
+    "url": "/flip-coin/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "51e2160b7a785d7385ee",
+    "url": "/flip-coin/static/js/2.fcca00e1.chunk.js"
+  },
+  {
+    "revision": "b5a01430b9928300ee798657f45fe7d4",
+    "url": "/flip-coin/static/js/2.fcca00e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faf143a2d91cf884c058",
+    "url": "/flip-coin/static/js/main.91eaeee1.chunk.js"
+  },
+  {
+    "revision": "e385f831e057cbaac9c4",
+    "url": "/flip-coin/static/js/runtime-main.07ecc3c9.js"
+  }
+]);
