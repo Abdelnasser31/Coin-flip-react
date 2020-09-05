596adee420dcd3e68d51958a8c77c602c0969cfd
Nasser <abdelnasser.kh10@gmail.com>
2020-09-05 14:56:04 +0300

    Updates

---
+++ precache-manifest.5c7475deccd36e78788cb0c921df7b16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aad34421cd5f7d4a38d921df6e1d5437",
+    "url": "/Coin-flip-react/index.html"
+  },
+  {
+    "revision": "1a9dd3c8ca3a078a1f9f",
+    "url": "/Coin-flip-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "51e2160b7a785d7385ee",
+    "url": "/Coin-flip-react/static/js/2.fcca00e1.chunk.js"
+  },
+  {
+    "revision": "b5a01430b9928300ee798657f45fe7d4",
+    "url": "/Coin-flip-react/static/js/2.fcca00e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a9dd3c8ca3a078a1f9f",
+    "url": "/Coin-flip-react/static/js/main.0ba27ef8.chunk.js"
+  },
+  {
+    "revision": "0448598dd502b65ff632",
+    "url": "/Coin-flip-react/static/js/runtime-main.a1b6ff64.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/flip-coin/precache-manifest.90c79d8d6e55884228130c7e05d19482.js"
+  "/Coin-flip-react/precache-manifest.5c7475deccd36e78788cb0c921df7b16.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/flip-coin/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Coin-flip-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
